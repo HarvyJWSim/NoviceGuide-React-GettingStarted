f76537c88ec17d085ce7e2223a5a3d7e40dacc9d
Harvy Sim Jia Wei <harvysimjw@gmail.com>
2018-04-26 15:59:03 +0800

    Update (1)-react-component-prop-state.js

--- (1)-react-component-prop-state.js
+++ (1)-react-component-prop-state.js
@@ -1,26 +1,22 @@
-const Subheading = (props) =>
-{
+const Subheading = (props) => {
 	return (
   	<h3>{props.title}</h3>
   );
 };
 
-class SubheadingWithState extends React.Component
-{
-  	state = { numOfDay: 99 };
+class SubheadingWithState extends React.Component {
+  state = { numOfDay: 99 };
 
-  	handleClickEvent = () =>
-	{
-		this.setState((prevState) => ({ numOfDay: prevState.numOfDay - 1 }));
-  	};
+  handleClickEvent = () => {
+  	this.setState((prevState) => ({ numOfDay: prevState.numOfDay - 1 }));
+  };
 
-	render()
-  	{
+	render() {
   	return (
-      		<h3 onClick={this.handleClickEvent}>
-        		Counting down {this.state.numOfDay} days to Annabelle birthday
-      		</h3>
-    	)};
+      <h3 onClick={this.handleClickEvent}>
+        Counting down {this.state.numOfDay} days to Annabelle birthday
+      </h3>
+  )};
 }
 
 //ReactDOM.render(<Subheading title="Hi Annabelle" />, mountNode);
